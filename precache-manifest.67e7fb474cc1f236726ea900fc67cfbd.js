cd88c99b4e848a554d9b607706332a3327e9c89e
Tim <tkolenda@gmail.com>
2019-07-10 13:49:37 -0400

    Updates

---
+++ precache-manifest.67e7fb474cc1f236726ea900fc67cfbd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14e35913ac7c04397fed176e45d0f386",
+    "url": "/timkolenda/shared-task-list/index.html"
+  },
+  {
+    "revision": "8706270af09398bd2eb5",
+    "url": "/timkolenda/shared-task-list/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/timkolenda/shared-task-list/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "8706270af09398bd2eb5",
+    "url": "/timkolenda/shared-task-list/static/js/main.b02e5cf8.chunk.js"
+  },
+  {
+    "revision": "d8ad73175c68769e1240",
+    "url": "/timkolenda/shared-task-list/static/js/runtime~main.70991122.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/timkolenda/shared-task-list/static/media/logo.5d5d9eef.svg"
+  }
+]);
